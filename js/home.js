8c8cca7292f3396eef7852c320c75ca7d450d50e
riadahmed24 <rilancer01@gmail.com>
2024-09-20 11:17:46 +0600

    added add money to the balance after pin verification

--- js/home.js
+++ js/home.js
@@ -3,6 +3,8 @@
  * Step-1: add event handler
  * prevent page reload after form submit
  * step-2: get money to be added to the account balance
+ * get the pin number
+ * step-3: verify the pin number
  */
 
 // step-1: add an event handler to the add money button inside the form
@@ -18,5 +20,26 @@ document
 
     // get the pin number provided
     const pinNumberInput = document.getElementById('input-pin-number').value;
-    console.log(pinNumberInput);
+    console.log(typeof pinNumberInput);
+
+    // step-3: verify the pin number
+    // wrong way to validate pin number
+    if (pinNumberInput === '1234') {
+      console.log('adding money to your account');
+
+      //   step-4: get the current balance
+      const balance = document.getElementById('account-balance').innerText;
+      console.log(typeof balance);
+
+      //   step-5: add addMoneyInput with balance
+      const addMoneyNumber = parseFloat(addMoneyInput);
+      const balanceNumber = parseFloat(balance);
+      const newBalance = addMoneyNumber + balanceNumber;
+      console.log(newBalance);
+
+      //   step-6: update the balance in the UI/DOM
+      document.getElementById('account-balance').innerText = newBalance;
+    } else {
+      alert('Failed to add money! Please try again');
+    }
   });
