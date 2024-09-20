c9d596c465d00182e8ca73cf560ef3f9cde62081
riadahmed24 <rilancer01@gmail.com>
2024-09-20 10:49:59 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,49 @@
+// console.log('button clicking file added')
+
+// document.getElementById('id').addEventListener('click', function)
+
+// search: form submit reloading the page
+
+// step-1 : set event handler
+
+// document
+//   .getElementById('button-login')
+//   .addEventListener('click', function (event) {
+//     // step-2 : prevent default behavior (prevent reloading bowser)
+//     event.preventDefault(); // <------ Vejal to Beginners
+
+//     // step-3 : get the phone number and the pin number
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+
+//     console.log(phoneNumber, pinNumber);
+
+//     // step-4 : validate phone and pin
+//     // this is temporary
+//     if (phoneNumber === '5' && pinNumber === '1234') {
+//       console.log('you are logged in');
+//       // step-5 : allow user to use the website
+//     } else {
+//       alert('Wrong phone number or pin');
+//     }
+//   });
+
+document
+  .getElementById('button-login')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // get phone number and pin
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+
+    // console.log(phoneNumber, pinNumber)
+
+    // bad way to validate
+    if (phoneNumber === '5' && pinNumber === '1234') {
+      console.log('You are logged in');
+      window.location.href = '/home.html';
+    } else {
+      alert('Wrong phone number or pin');
+    }
+  });
